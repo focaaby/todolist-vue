ef13f5b2e6e676c95757cf1d6995cade37d53c28
Jerry Wang <focaaby@gmail.com>
2017-04-10 18:26:09 +0800

    Init project

---
+++ src/components/Hello.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <h1>Todos</h1>
+    <input type="text" v-model="message" v-on:keyup.enter="addtodo">
+
+    <ul id="example-1">
+      <li v-for="todo in todos">
+        <span>{{ todo.created_at | moment("LLL") }}</span>
+        {{ todo.content }}
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'hello',
+  data() {
+    return {
+      message: '',
+      todos: [],
+    };
+  },
+  created() {
+    this.gettodos();
+  },
+  methods: {
+    gettodos() {
+      axios.get('http://localhost:3000/todos')
+      .then((res) => {
+        this.todos = res.data;
+      }).catch((err) => {
+        console.log(err);
+      });
+    },
+    addtodo() {
+      axios.post('http://localhost:3000/todos', {
+        content: this.message,
+      }).then(() => {
+        this.gettodos();
+      }).catch((err) => {
+        console.log(err);
+      });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
